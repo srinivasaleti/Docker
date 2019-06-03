af2959cfb269d3820b7f38860ef05d99c52f10ac
Aleti Srinivasrao <alti@Administrators-MacBook-Pro.local>
2019-06-03 13:54:26 +0530

    Get books from backend render

--- simple-react/src/App.js
+++ simple-react/src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { backend_url } from './config';
 
-function App() {
-  return <div className='App'>"Hello World"</div>;
+class App extends React.Component {
+  state = { movies: [] };
+  componentDidMount = async () => {
+    const resp = await fetch(backend_url + '/movies', { method: 'GET' });
+    const data = await resp.json();
+    this.setState({
+      movies: data
+    });
+  };
+  render() {
+    return (
+      <div className='App'>
+        {this.state.movies.map((x, index) => (
+          <li key={index}>{x.name + ' ' + x.hero}</li>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
