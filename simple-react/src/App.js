957611947d6c2c4f2e34b32e09d433eac0d453c5
Aleti Srinivasrao <alti@Administrators-MacBook-Pro.local>
2019-06-10 15:29:02 +0530

    Render only hello world

--- simple-react/src/App.js
+++ simple-react/src/App.js
@@ -3,20 +3,21 @@ import { backend_url } from './config';
 
 class App extends React.Component {
   state = { movies: [] };
-  componentDidMount = async () => {
-    const resp = await fetch(backend_url + '/movies', { method: 'GET' });
-    const data = await resp.json();
-    this.setState({
-      movies: data
-    });
-  };
+  // componentDidMount = async () => {
+  //   const resp = await fetch(backend_url + '/movies', { method: 'GET' });
+  //   const data = await resp.json();
+  //   this.setState({
+  //     movies: data
+  //   });
+  // };
   render() {
     return (
-      <div className='App'>
-        {this.state.movies.map((x, index) => (
-          <li key={index}>{x.name + ' ' + x.hero}</li>
-        ))}
-      </div>
+      <div>Hello World</div>
+      // <div className='App'>
+      //   {this.state.movies.map((x, index) => (
+      //     <li key={index}>{x.name + ' ' + x.hero}</li>
+      //   ))}
+      // </div>
     );
   }
 }
